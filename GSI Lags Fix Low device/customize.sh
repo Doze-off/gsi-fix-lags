f22f951bbe7f20361129ed198c98e4f091f7d2a1
Ned <viniciussadxxx@gmail.com>
2025-03-30 23:00:03 -0300

    Add files via upload

--- GSI Lags Fix Low device/customize.sh
+++ GSI Lags Fix Low device/customize.sh
@@ -10,17 +10,7 @@ ui_print ""
 ui_print " • Iniciando a instalação"
 sleep 2
 ui_print ""
-ui_print " • Verificando o governador de CPU..."
-CPU_GOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-
-if [ "$CPU_GOV" = "schedutil" ]; then
-    ui_print ""
-    ui_print " • Governador 'schedutil' encontrado "
-    ui_print " • aplicando configurações. "
-else
-    ui_print " • Governador 'schedutil' não encontrado. "
-    ui_print " • Usando 'interactive' como alternativa. "
-fi
+ui_print " • Aplicando Tweaks"
 sleep 0.5
 ui_print ""
 ui_print " • Instalação concluída com sucesso! "
