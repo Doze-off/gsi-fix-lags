c3e92a2c6d65462ded12f009e7be34aba6cfb200
Papacu <viniciussadxxx@gmail.com>
2025-04-25 04:20:30 -0300

    Update customize.sh

--- GSI Lags Fix Low device/customize.sh
+++ GSI Lags Fix Low device/customize.sh
@@ -1,14 +1,14 @@
-
-sleep 1
 ui_print " • Informações do módulo "
 sleep 0.2
 ui_print " • Nome            : GSI Lags Fix"
 sleep 0.2
-ui_print " • Desenvolvedor   : @NedTOP "
+ui_print " • Desenvolvedor   : @Papacuz "
+sleep 0.2
+ui_print " • Canal.           : @@Papacu_GSIs_MODs "
 sleep 0.2
 ui_print ""
 ui_print " • Iniciando a instalação"
-sleep 2
+sleep 1
 ui_print ""
 ui_print " • Aplicando Tweaks"
 sleep 0.5
